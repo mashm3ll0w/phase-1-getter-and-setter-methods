f57eaec00e044b8149f207463257d0badd8e45d1
Charles Swaleh <charlswall@live.com>
2022-12-31 13:32:09 +0300

    Completed the setter and getter methods lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Add your Circle class here
+
+class Circle{
+  constructor(radius){
+    this.radius = radius
+  }
+  
+  get diameter(){
+    return this.radius * 2
+  }
+
+  get circumference(){
+    return Math.PI * this.diameter
+  }
+
+  get area(){
+    return Math.PI * (this.radius * this.radius)
+  }
+
+  set diameter(newDiameter){
+    this.radius = newDiameter / 2
+  }
+
+  set circumference(newCircumference){
+    this.radius = (newCircumference / Math.PI) / 2
+  }
+
+  set area(newArea){
+    this.radius = Math.sqrt(newArea / Math.PI)
+  }
+}
+
+
+// Test data
+
+const circle = new Circle(10)
+circle.circumference = 100;
+console.log(circle.radius)
+console.log(circle.diameter)
+console.log(circle.area)
